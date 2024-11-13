e64ddabe0b7b4923a984dd9b70e0ecb1d9d552b6
aheebwa32 <aheebwasteve3@gmail.com>
2024-11-13 11:53:15 +0300

    added the Response List

---
+++ frontend/src/components/promptResponseList/response-interface.ts
@@ -0,0 +1,29 @@
+interface TemporalContext {
+    info_date?: string;
+    history_context: string;
+  }
+  
+  interface MainResponse {
+    summary: string;
+    detailed_info: string;
+    key_points: string[];
+  }
+  
+  interface TextContent {
+    temporal_context?: TemporalContext;
+    main_response?: MainResponse;
+    disclaimer?: string;
+  }
+  
+  export interface ApiResponse {
+    text: TextContent;
+    citation?: any;
+  }
+  
+  export interface ResponseInterface {
+    id: string;
+    response?: ApiResponse;
+    selfFlag: boolean;
+    error?: boolean;
+    image?: string;
+  }
